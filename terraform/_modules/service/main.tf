018dfd43457418edb1cf35a785f6f678ce8bde21
sky0621 <sky14work@gmail.com>
2025-04-20 18:13:29 +0900

    -

--- terraform/_modules/service/main.tf
+++ terraform/_modules/service/main.tf
@@ -3,6 +3,13 @@ resource "google_project_service" "service_usage" {
   service = "serviceusage.googleapis.com"
 }
 
+resource "google_project_service" "iam_credentials" {
+  project            = var.project_id
+  service            = "iamcredentials.googleapis.com"
+  disable_on_destroy = false
+  depends_on         = ["google_project_service.service_usage"]
+}
+
 resource "google_project_service" "artifact_registry" {
   project            = var.project_id
   service            = "artifactregistry.googleapis.com"
