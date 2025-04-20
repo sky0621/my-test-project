74467f2c36c5ccd329ca314b0881895036206738
sky0621 <sky14work@gmail.com>
2025-04-20 18:07:24 +0900

    -

--- terraform/_modules/iam_workload_identity_pool_provider/main.tf
+++ terraform/_modules/iam_workload_identity_pool_provider/main.tf
@@ -8,15 +8,16 @@ resource "google_iam_workload_identity_pool_provider" "default" {
   workload_identity_pool_id          = google_iam_workload_identity_pool.default.workload_identity_pool_id
   workload_identity_pool_provider_id = var.workload_identity_pool_provider_id
 
-  attribute_mapping = {
-    "google.subject"       = "assertion.sub"
-    "attribute.actor"      = "assertion.actor"
-    "attribute.repository" = "assertion.repository"
-  }
-
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
 
-  attribute_condition = "attribute.repository == assertion.repository && attribute.repository_owner == assertion.repository_owner"
+  attribute_mapping = {
+    "google.subject" = "assertion.sub"
+    # "attribute.actor"      = "assertion.actor"
+    "attribute.repository" = "assertion.repository"
+  }
+
+  # attribute_condition = "attribute.repository == assertion.repository && attribute.repository_owner == assertion.repository_owner"
+  attribute_condition = "attribute.repository == assertion.repository"
 }
